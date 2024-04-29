e713de6e469afcf984532fb760833464775c4b1d
Matars <khaledmatar19733@gmail.com>
2024-04-29 23:56:21 +0200

    first commit

---
+++ app/api/frame/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+async function getResponse(req: NextRequest): Promise<NextResponse> {
+  const searchParams = req.nextUrl.searchParams
+  const id:any = searchParams.get("id")
+  const idAsNumber = parseInt(id)
+
+  const nextId = idAsNumber + 1
+
+  if(idAsNumber === 3){
+      return new NextResponse(`<!DOCTYPE html><html><head>
+    <title>This is frame 7</title>
+    <meta property="fc:frame" content="vNext" />
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmZ46XKoNr3y7RhTJi7GsD28sbZXFdY3vhgKPVHmtQD55P/3.png" />
+    <meta property="fc:frame:button:1" content="Visit CosmicCowboys.cloud" />
+    <meta property="fc:frame:button:1:action" content="post_redirect" />
+    <meta property="fc:frame:button:2" content="Learn How this was made" />
+    <meta property="fc:frame:button:2:action" content="post_redirect" />
+    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end" />
+  </head></html>`);
+  } else {
+  return new NextResponse(`<!DOCTYPE html><html><head>
+    <title>This is frame ${id}</title>
+    <meta property="fc:frame" content="vNext" />
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmZ46XKoNr3y7RhTJi7GsD28sbZXFdY3vhgKPVHmtQD55P/${id}.png" />
+    <meta property="fc:frame:button:1" content="Next Page" />
+    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${nextId}" />
+  </head></html>`);
+  }
+}
+
+export async function POST(req: NextRequest): Promise<Response> {
+  return getResponse(req);
+}
+
+export const dynamic = 'force-dynamic';
